c0e3b711c19529a68f5ab60583ecca26cca5251b
Kamogelo5298 <KMatsomela@retrorabbit.co.za>
2023-07-25 14:18:49 +0200

    Adding actoin for getting the type of user

--- Frontend/RGO-Frontend/src/app/store/actions/app.actions.ts
+++ Frontend/RGO-Frontend/src/app/store/actions/app.actions.ts
@@ -3,5 +3,6 @@ import { Token } from "../../models/token.interface";
 import { Events } from 'src/app/models/events.interface';
 
 export const GetLogin = createAction('[signin] UserLogin', props<{ payload: Token }>());
+export const GetType = createAction('[signin] GetType');
 export const GetEvents = createAction('[gradtodo] Get Events', props<{ token: string }>());
 export const GetEventsSuccess = createAction('[gradtodo] Get Events Success', props<{ events: Events[] }>());
