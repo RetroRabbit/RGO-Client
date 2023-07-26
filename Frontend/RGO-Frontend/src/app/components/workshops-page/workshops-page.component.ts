335c6d6f945a0fe297134562e9757947407060fe
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-07-26 16:08:48 +0200

    Added Workshop Compomnent

---
+++ Frontend/RGO-Frontend/src/app/components/workshops-page/workshops-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workshops-page',
+  templateUrl: './workshops-page.component.html',
+  styleUrls: ['./workshops-page.component.css']
+})
+export class WorkshopsPageComponent {
+
+}
