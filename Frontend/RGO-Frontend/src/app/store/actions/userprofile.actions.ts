db9c47565a5640adea944138a4711f4736f69c2e
CarlWehl <Cwehl@retrorabbit.co.za>
2023-07-28 07:35:06 +0200

    NgRx Fixes

--- Frontend/RGO-Frontend/src/app/store/actions/userprofile.actions.ts
+++ Frontend/RGO-Frontend/src/app/store/actions/userprofile.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { Token } from "../../models/token.interface";
 
 
-export const GetUserProfile = createAction('[userprofile] get user', props<{ user: any }>());
+export const GetUserProfile = createAction('[userprofile] get user', props<{  user: any, token: string }>());
--- Frontend/RGO-Frontend/src/app/store/effects/userprofile.effects.ts
+++ Frontend/RGO-Frontend/src/app/store/effects/userprofile.effects.ts
@@ -2,19 +2,21 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { mergeMap } from 'rxjs/operators';
 import { EMPTY, Observable } from "rxjs";
+import { UserprofileService } from "src/app/services/userprofile.service";
 import * as UserProfile from '../actions/userprofile.actions';
 
 @Injectable()
 export class UserProfileEffects{
   constructor(
-    private actions$: Actions
+    private actions$: Actions,
+    private userprofileService: UserprofileService,
   ) { }
 
   GetLogin$ = createEffect(() =>
     this.actions$.pipe(
         ofType(UserProfile.GetUserProfile),
-        mergeMap(({user}) => {
-          UserProfile.GetUserProfile({user})
+        mergeMap(({token}) => {
+          this.userprofileService.GetUserProfile({token})
             return new Observable<any>();
         })
     )
