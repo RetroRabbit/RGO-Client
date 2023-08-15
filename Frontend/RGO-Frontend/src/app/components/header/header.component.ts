28ea69c42392bec5abd57a353276204b4d82a242
VhulendaNdou <vndou@retrorabbit.co.za>
2023-08-15 16:36:03 +0200

    Added html and js from material dashboard template

--- Frontend/RGO-Frontend/src/app/components/header/header.component.ts
+++ Frontend/RGO-Frontend/src/app/components/header/header.component.ts
@@ -1,17 +1,41 @@
-import { Component, ElementRef, OnDestroy } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { ROUTES } from '../sidebar/sidebar.component';
+import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnDestroy {
+export class HeaderComponent implements OnDestroy, OnInit {
   currentTime = new Date();
-  sidebarVisible = false;
-  navbar: HTMLElement = this.element.nativeElement;
+  private listTitles !: any[];
+    location: Location;
+      mobile_menu_visible: any = 0;
+    private toggleButton: any;
+    private sidebarVisible: boolean;
+    $layer: any;
 
-  constructor(private auth: AuthService, private element: ElementRef) {}
+  constructor(private auth: AuthService, location: Location,  private element: ElementRef, private router: Router) {
+    this.location = location;
+          this.sidebarVisible = false;
+  }
+
+  ngOnInit(): void {
+    this.listTitles = ROUTES.filter(listTitle => listTitle);
+      const navbar: HTMLElement = this.element.nativeElement;
+      this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+      this.router.events.subscribe((event) => {
+        this.sidebarClose();
+         this.$layer = document.getElementsByClassName('close-layer')[0];
+         if (this.$layer) {
+           this.$layer.remove();
+           this.mobile_menu_visible = 0;
+         }
+     });
+  }
 
   ngOnDestroy() {
     localStorage.removeItem('access_token');
@@ -26,30 +50,78 @@ export class HeaderComponent implements OnDestroy {
     });
   }
   
-  sidebarToggle(){
-    if (this.sidebarVisible === false) {
-      this.sidebarOpen();
-    } else {
-        this.sidebarClose();
-    }
-  }
-
   sidebarOpen() {
-    
-    const toggleButton = this.navbar.getElementsByClassName('navbar-toggler')[0];
+    const toggleButton = this.toggleButton;
     const body = document.getElementsByTagName('body')[0];
     setTimeout(function(){
         toggleButton.classList.add('toggled');
     }, 500);
-    body.classList.add('nav-open');
-    this.sidebarVisible = true;
-  };
 
-  sidebarClose() {
-      const body = document.getElementsByTagName('body')[0];
-      const toggleButton = this.navbar.getElementsByClassName('navbar-toggler')[0];
-      toggleButton.classList.remove('toggled');
-      this.sidebarVisible = false;
-      body.classList.remove('nav-open');
+    body.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+};
+sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+};
+sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    var $toggle = document.getElementsByClassName('navbar-toggler')[0];
+
+    if (this.sidebarVisible === false) {
+        this.sidebarOpen();
+    } else {
+        this.sidebarClose();
+    }
+    const body = document.getElementsByTagName('body')[0];
+
+    if (this.mobile_menu_visible == 1) {
+        // $('html').removeClass('nav-open');
+        body.classList.remove('nav-open');
+        if (this.$layer) {
+            this.$layer.remove();
+        }
+        setTimeout(function() {
+            $toggle.classList.remove('toggled');
+        }, 400);
+
+        this.mobile_menu_visible = 0;
+    } else {
+        setTimeout(function() {
+            $toggle.classList.add('toggled');
+        }, 430);
+
+        var $layer = document.createElement('div');
+        $layer.setAttribute('class', 'close-layer');
+
+
+        if (body.querySelectorAll('.main-panel')) {
+            document.getElementsByClassName('main-panel')[0].appendChild($layer);
+        }else if (body.classList.contains('off-canvas-sidebar')) {
+            document.getElementsByClassName('wrapper-full-page')[0].appendChild($layer);
+        }
+
+        setTimeout(function() {
+            $layer.classList.add('visible');
+        }, 100);
+
+        $layer.onclick = () => { //asign a function
+          body.classList.remove('nav-open');
+          this.mobile_menu_visible = 0;
+          $layer.classList.remove('visible');
+          setTimeout(function() {
+              $layer.remove();
+              $toggle.classList.remove('toggled');
+          }, 400);
+        };
+
+        body.classList.add('nav-open');
+        this.mobile_menu_visible = 1;
+
+    }
   };
 }
