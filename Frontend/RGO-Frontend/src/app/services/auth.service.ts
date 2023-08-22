fb928259382443f6be6372d4e0b4ff600e9be950
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-08-22 14:16:40 +0000

    Apply suggestions from code review

--- Frontend/RGO-Frontend/src/app/services/auth.service.ts
+++ Frontend/RGO-Frontend/src/app/services/auth.service.ts
@@ -30,7 +30,6 @@ export class AuthService {
     .pipe(
       map(type => type),
       catchError(err => {
-        console.log(err)
         if (err.status == 404) {
           window.alert("Contact admin to create your account")
         }
