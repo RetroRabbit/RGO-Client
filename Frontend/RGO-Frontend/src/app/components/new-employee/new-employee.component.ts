f1bf7cec1e316e5214032a0286a16648a7d834ca
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-09-20 08:28:26 +0000

    Apply suggestions from code review

--- Frontend/RGO-Frontend/src/app/components/new-employee/new-employee.component.ts
+++ Frontend/RGO-Frontend/src/app/components/new-employee/new-employee.component.ts
@@ -48,9 +48,7 @@ export class NewEmployeeComponent implements OnInit {
   onSubmit(){
     this.newEmployeeForm.value.cellphoneNo = this.newEmployeeForm.value.cellphoneNo?.toString();
     this.employeeService.addEmployee(this.newEmployeeForm.value).subscribe({
-      next: (data) => {
-        console.table(data);
-      },
+      next: (data) => { },
       error: (error) => {
         console.table(error);
       }
