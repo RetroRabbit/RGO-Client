16934bb00d7d14361d94f5d84ec62e7be7500d77
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-09-29 10:11:07 +0200

    Made changes to chart interface

--- Frontend/RGO-Frontend/src/app/models/charts.interface.ts
+++ Frontend/RGO-Frontend/src/app/models/charts.interface.ts
@@ -1,8 +1,8 @@
 export interface Chart {
-    Id: number;
-    Name: string;
-    Type: string;
-    DataType:string;
-    Labels: string[];
-    Data: number[];
+    id: number;
+    name: string;
+    type: string;
+    dataType:string;
+    labels: string[];
+    data: number[];
   }
