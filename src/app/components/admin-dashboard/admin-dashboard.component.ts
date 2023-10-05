06b8686aeb54a37c2408dd24a7cbcd2a871050f2
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-05 16:14:23 +0200

    added dynamic data

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { EmployeeProfileService } from 'src/app/services/employee-profile.service';
+import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
+import { Chart } from 'src/app/models/charts.interface';
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
+
 export class AdminDashboardComponent {
   selectedItem: string = 'Dashboard';
-  menuClicked : boolean =  false;
+  menuClicked: boolean = false;
+  admin !: EmployeeProfile;
+  profileImage : string = '';
+  charts: Chart[] = [];
+  constructor(private employeeProfileService: EmployeeProfileService) { }
+  ngOnInit() {
+    this.employeeProfileService.GetEmployeeProfile().subscribe(data => {
+      this.admin = data;
+      this.profileImage = this.admin.photo;
+      this.getPhoto();
+    });
+   // this.chartService.getAllCharts().subscribe(data => this.charts = data);
+  }
+
+  getPhoto() {
+      
+  }
 }
