9f348e923bb72f67ac179bc885b789d3b76a1d8c
Dorothy Mahoko <mahokod@fourier.co.za>
2023-10-05 09:22:31 +0200

    evaluating evaluations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,25 +17,24 @@ import { LoginEffects } from './store/effects/app.effects';
 import { AuthService } from './services/auth.service';
 import { AuthInterceptor } from './interceptor/auth0.interceptor';
 import { UserReducer } from './store/reducers/user.reducer';
-import { UserEffects } from './store/effects/user.effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EmployeeProfileComponent } from './components/employee-profile/employee-profile.component';
+import { EmployeeProfileComponent } from './components/employee/employee-profile/employee-profile.component';
 import { EmployeeProfileReducer } from './store/reducers/employee-profile.reducer';
 import { EmployeeProfileEffects } from './store/effects/employeeprofile.effects';
 import { NgChartsModule } from 'ng2-charts';
 import { ChartComponent } from './components/charts/charts.component';
-import { CreateChartsComponent } from './components/create-charts/create-charts.component';
+import { CreateChartsComponent } from './components/charts/create-charts/create-charts.component';
 import { NgToastModule } from 'ng-angular-popup';
 import { ReportComponent } from './components/chart-reports/chart-reports.component';
 import { RoleManagerComponent } from './components/role-manager/role-manager.component';
-import { EmployeeRoleManagerComponent } from './components/employee-role-manager/employee-role-manager.component';
-import { NewEmployeeComponent } from './components/new-employee/new-employee.component';
+import { EmployeeRoleManagerComponent } from './components/employee/employee-role-manager/employee-role-manager.component';
+import { NewEmployeeComponent } from './components/employee/new-employee/new-employee.component';
 import { ManageFieldCodeComponent } from './components/manage-field-code/manage-field-code.component';
-import { ViewEmployeeComponent } from './components/view-employee/view-employee.component';
+import { ViewEmployeeComponent } from './components/employee/view-employee/view-employee.component';
 import { NewFieldCodeComponent } from './components/new-field-code/new-field-code.component';
 import { UpdateFieldComponent } from './components/update-field/update-field.component';
-import { EvaluationsComponent } from './components/evaluations/evaluations.component';
-import { EmployeeEvaluationsComponent } from './components/employee-evaluations/employee-evaluations.component';
+import { EvaluationsComponent } from './components/employee/evaluations/evaluations.component';
+import { EmployeeEvaluationsComponent } from './components/employee/employee-evaluations/employee-evaluations.component';
 import { TemplateItemsComponent } from './components/template-items/template-items.component';
 
 @NgModule({
@@ -71,7 +70,7 @@ import { TemplateItemsComponent } from './components/template-items/template-ite
       employee : EmployeeProfileReducer,
       users: UserReducer,
     }),
-    EffectsModule.forRoot([LoginEffects, UserEffects, EmployeeProfileEffects]),
+    EffectsModule.forRoot([LoginEffects, EmployeeProfileEffects]),
     AuthModule.forRoot({
       domain: environment.AUTH0_Domain_key, // domain
       clientId: environment.AUTH0_CLIENT_ID, // clientId
