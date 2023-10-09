0eaf71eecbd79a5aed429cfa6b5992d44e99b2e2
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-10-09 16:00:13 +0200

    Removing extra imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,11 +37,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
