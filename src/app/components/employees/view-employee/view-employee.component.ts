727dbe40ea32956ef1dcacd0893e987317ddb261
ksabjee <ksabjee@retrorabbit.co.za>
2023-10-09 12:47:17 +0200

    HR view employee details
    HR view employee details

--- src/app/components/employees/view-employee/view-employee.component.ts
+++ src/app/components/employees/view-employee/view-employee.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
 import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,11 +12,13 @@ import { CookieService } from 'ngx-cookie-service';
 })
 
 export class ViewEmployeeComponent{
-  @Output() selectedItem = new EventEmitter<{ selectedPage: string }>();
+  // @Output() selectedItem = new EventEmitter<{ selectedPage: string }>();
+  @Output() selectedEmployee = new EventEmitter<any>()
   Employees: EmployeeProfile[] = [];
+  selectedEmp: any;
 
   constructor(private employeeService: EmployeeService,
-    private cookieService: CookieService) { }
+    private cookieService: CookieService, private router: Router) { }
 
   ngOnInit() {
     this.getEmployees();
@@ -31,10 +34,19 @@ export class ViewEmployeeComponent{
 
   CaptureEvent(event: any) {
     const target = event.target as HTMLAnchorElement;
+    console.log(target.innerText);
     this.cookieService.set('currentPage', target.innerText);
   }
 
   ViewUser(email: string){
     this.cookieService.set('selectedUser',email)
   }
+
+  employeeClickEvent(event: any, employee: EmployeeProfile): void{
+    this.selectedEmp = employee
+    this.selectedEmployee.emit(this.selectedEmp)
+    this.cookieService.set('currentPage', 'Employee Details');
+  }
+  
+
 }
---
+++ src/app/models/constants/race.constants.ts
@@ -0,0 +1,7 @@
+export const race = [
+    {id: 0, value: 'Black'},
+    {id: 1, value: 'White'},
+    {id: 2, value: 'Indian'},
+    {id: 3, value: 'Coloured'},
+    {id: 4, value: 'Asian'}
+]
