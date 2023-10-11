0b350bc25a9456139b531f66b12ba63321091e18
Dorothy Mahoko <mahokod@fourier.co.za>
2023-10-11 15:56:36 +0200

    clean up and conditional nav

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,8 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { Chart } from 'src/app/models/charts.interface';
 import { ChartService } from 'src/app/services/charts.service';
 import { AuthService } from '@auth0/auth0-angular';
 import { CookieService } from 'ngx-cookie-service';
-import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
-import { EmployeeProfileService } from 'src/app/services/employee/employee-profile.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -16,7 +14,6 @@ export class AdminDashboardComponent {
 
   selectedItem: string = 'Dashboard';
   menuClicked: boolean = false;
-  admin!: EmployeeProfile;
   profileImage: string | null = null;
 
   employeeType: { id: number; name: string } = {
@@ -25,36 +22,17 @@ export class AdminDashboardComponent {
   };
   
   constructor(
-    private employeeProfileService: EmployeeProfileService,
     private chartService: ChartService,
     private auth: AuthService,
     private cookieService: CookieService
   ) { }
 
   ngOnInit() {
-    this.employeeProfileService.GetEmployeeProfile().subscribe((data) => {
-      this.admin = data;
-
-      if (
-        this.admin.photo &&
-        (this.admin.photo.startsWith('http') ||
-          this.admin.photo.startsWith('data:image'))
-      ) {
-        this.profileImage = this.admin.photo;
-      }
-      this.employeeType = this.admin.employeeType;
-    });
-
     this.chartService.getAllCharts().subscribe({
       next: (data) => (this.charts = data),
     });
   }
 
-  logout() {
-    this.auth.logout({
-      logoutParams: { returnTo: document.location.origin },
-    });
-  }
 
   CaptureEvent(event: any) {
     const target = event.target as HTMLAnchorElement;
