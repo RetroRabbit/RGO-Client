d4d6b5ed7233da94033db317ce6e236e6b4ea475
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-17 17:03:05 +0200

    Accordion nav

--- src/app/components/employees/employee-profile/employee-profile.component.ts
+++ src/app/components/employees/employee-profile/employee-profile.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AccessPropertiesService } from 'src/app/services/access-properties.service';
 import { Properties } from 'src/app/models/properties.interface';
 import { CookieService } from 'ngx-cookie-service';
+import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
+import { EmployeeProfileService } from 'src/app/services/employee/employee-profile.service';
 @Component({
   selector: 'app-employee-profile',
   templateUrl: './employee-profile.component.html',
@@ -10,69 +12,32 @@ import { CookieService } from 'ngx-cookie-service';
 export class EmployeeProfileComponent {
   EmployeeFields: Properties[] = [];
   EditFields: Properties[] = [];
-
+  EmployeeProfile !: EmployeeProfile;
   isEdit: boolean = false;
-  
-  name : string = "";
-  surname : string = "";
-  pc : string = "";
+  selectedItem: string = 'Profile Details';
 
-  constructor(private accessPropertyService: AccessPropertiesService, 
-    private cookieService : CookieService) { }
+  constructor(private accessPropertyService: AccessPropertiesService,
+    private cookieService: CookieService,
+    private employeeProfileService : EmployeeProfileService) { }
 
   ngOnInit() {
     this.getEmployeeFields();
   }
 
-  getEmployeeFields(){
+  getEmployeeFields() {
     this.accessPropertyService.GetAccessProperties(this.cookieService.get('userEmail')).subscribe({
       next: data => {
         this.EmployeeFields = data;
-        console.log(this.EmployeeFields)
-        this.name = this.getValue("Employee Name");
-        this.surname = this.getValue("Surname");
-        this.pc = this.getValue("People's Champion");
-        console.log(this.pc)
-      }
-  });
-  }
-
-  toggleEdit() {
-    this.isEdit = !this.isEdit;
-    this.EditFields = JSON.parse(JSON.stringify(this.EmployeeFields));
-  }
-
-  canEdit(field: Properties): boolean{
-    return this.isEdit && field.condition == 2;
-  }
-
-  captureChange(htmlValue: any, index: number) {
-    this.EditFields[index].value = htmlValue.target.value;
-  }
-
-  saveChanges(){
-    let payload : any[] = [];
-
-    this.EditFields.forEach((field, index) =>{
-      if(field.value !== this.EmployeeFields[index].value){
-        payload.push({
-          fieldId: field.id,
-          value: field.value
-        })
       }
     });
-    this.accessPropertyService.UpdateProperties(this.cookieService.get('userEmail'), payload).subscribe(
-      () => {
+    this.employeeProfileService.GetEmployeeProfile().subscribe({
+      next: data => {
+        this.EmployeeProfile = data;
+      }
     });
-    setTimeout(()=>{
-      this.getEmployeeFields();
-    },500)
-    this.toggleEdit();
   }
-
-    getValue(label : any) : any{
-      let field = this.EmployeeFields.filter( field => field.label === label);
-      // console.table(field);
-      return field[0].value != null ? field[0].value : "-";
-    }
+  CaptureEvent(event: any) {
+    const target = event.target as HTMLAnchorElement;
+    this.selectedItem = target.innerText;
+  }
 }
