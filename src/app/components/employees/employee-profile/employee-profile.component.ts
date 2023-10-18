27db02dc01f1b25fc690eeb4a73020e0b45b11e2
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-18 15:23:17 +0200

    Added edit and cancel button

--- src/app/components/employees/employee-profile/employee-profile.component.ts
+++ src/app/components/employees/employee-profile/employee-profile.component.ts
@@ -4,6 +4,11 @@ import { Properties } from 'src/app/models/properties.interface';
 import { CookieService } from 'ngx-cookie-service';
 import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
 import { EmployeeProfileService } from 'src/app/services/employee/employee-profile.service';
+import { race } from 'src/app/models/constants/race.constants';
+import { gender } from 'src/app/models/constants/gender.constants';
+import { tshirtSize } from 'src/app/models/constants/tshirt.constants';
+import { nationalities } from 'src/app/models/constants/nationaility.constants';
+import { disabilities } from 'src/app/models/constants/disabilities.constant';
 @Component({
   selector: 'app-employee-profile',
   templateUrl: './employee-profile.component.html',
@@ -15,16 +20,27 @@ export class EmployeeProfileComponent {
   EmployeeProfile !: EmployeeProfile;
   isEdit: boolean = false;
   selectedItem: string = 'Profile Details';
-  expandedIndex = 0;
-  
+  expandedIndex = 0;  
   panelOpenState : boolean = false;
+  races : any[] = [];
+  genders : any[] = [];
+  sizes : any[] = [];
+  nationalities : any[] = [];
+  hasDisbility : boolean = false;
+  disabilities: any[] = [];
 
+  editPersonal : boolean = false;
   constructor(private accessPropertyService: AccessPropertiesService,
     private cookieService: CookieService,
     private employeeProfileService : EmployeeProfileService) { }
 
   ngOnInit() {
     this.getEmployeeFields();
+    this.races = race;
+    this.genders = gender;
+    this.sizes = tshirtSize;
+    this.nationalities = nationalities;
+    this.disabilities = disabilities;
   }
 
   getEmployeeFields() {
@@ -42,5 +58,17 @@ export class EmployeeProfileComponent {
   CaptureEvent(event: any) {
     const target = event.target as HTMLAnchorElement;
     this.selectedItem = target.innerText;
+  }
+
+  setHasDisability(event: any) {
+      this.hasDisbility = event.value;
+  }
+
+  editPersonalDetails(){
+    this.editPersonal = true;
+  }
+  cancelPersonalEdit(){
+    this.editPersonal = false;
+    this.hasDisbility = false;
   }
 }
