b334ff3aa805c2aa874300464c7e355b78d91017
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-10-18 15:23:17 +0200

    Added edit and cancel button

---
+++ src/app/models/constants/tshirt.constants.ts
@@ -0,0 +1,10 @@
+export const tshirtSize = [
+    {id: 0, value: 'None'},
+    {id: 1, value: 'XS'},
+    {id: 2, value: 'S'},
+    {id: 3, value: 'M'},
+    {id: 4, value: 'L'},
+    {id: 5, value: 'XL'},
+    {id: 6, value: 'XXL'},
+    {id: 7, value: 'XXXL'}
+]
