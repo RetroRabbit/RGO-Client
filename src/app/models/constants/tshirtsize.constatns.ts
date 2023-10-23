7bb5ac2034b470ad8f85cc5337675c535bda000b
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-10-23 09:22:58 +0200

    removing unused models

--- src/app/models/constants/tshirtsize.constatns.ts
+++
@@ -1,9 +0,0 @@
-export const tshirtsizes = [
-    'XS',
-    'S',
-    'M',
-    'L',
-    'XL',
-    'XXL',
-    'XXXL',
-]
--- src/app/models/dietary-controls.interface.ts
+++
@@ -1,5 +0,0 @@
-import { FormControl } from "@angular/forms";
-
-export interface DietaryControls {
-  dietary: FormControl<string | null>;
-}
