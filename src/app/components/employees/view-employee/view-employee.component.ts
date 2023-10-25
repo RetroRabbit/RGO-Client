790da4fbe5d77349287e5c7b9ba1e3cc7b593c5a
Kamogelo Matsomela <kmatsomela@retrorabbit.co.za>
2023-10-25 14:51:46 +0200

    Initial redesign

--- src/app/components/employees/view-employee/view-employee.component.ts
+++ src/app/components/employees/view-employee/view-employee.component.ts
@@ -1,47 +1,156 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewChild, HostListener } from '@angular/core';
 import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
 import { CookieService } from 'ngx-cookie-service';
-
+import { Observable, first, map } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { EmployeeRoleService } from 'src/app/services/employee/employee-role.service';
 
 @Component({
   selector: 'app-view-employee',
   templateUrl: './view-employee.component.html',
-  styleUrls: ['./view-employee.component.css']
+  styleUrls: ['./view-employee.component.css'],
 })
-
-export class ViewEmployeeComponent{
-  @Output() selectedEmployee = new EventEmitter<any>()
+export class ViewEmployeeComponent {
+  @Output() selectedEmployee = new EventEmitter<any>();
+  @Output() viewEmployee = new EventEmitter<boolean>();
+  searchTerm: string = '';
+  filteredEmployees$: Observable<EmployeeProfile[]> =
+    this.employeeService.getAllProfiles();
   Employees: EmployeeProfile[] = [];
   selectedEmp: any;
 
-  constructor(private employeeService: EmployeeService,
-    private cookieService: CookieService) { }
-
-  ngOnInit() {
-    this.getEmployees();
+  get filteredEmployees(): EmployeeProfile[] {
+    return this.Employees.filter((e) =>
+      (e.name + ' ' + e.surname)
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase())
+    );
   }
 
-  getEmployees(){
-    this.employeeService.getAllProfiles().subscribe({
-      next: data => {
-        this.Employees = data;
-      }
-  });
+  constructor(
+    private employeeService: EmployeeService,
+    private employeeRoleService: EmployeeRoleService,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit() {
+    this.onResize();
+    this.getEmployees();
+
+    console.log(this.paginator, this.dataSource.paginator)
+  }
+
+  getEmployees() {
+    this.employeeService
+      .getAllProfiles()
+      .pipe(
+        map((employees: EmployeeProfile[]) => {
+          return employees.map((emp: EmployeeProfile) => {
+            const roles: string[]= [];
+            this.employeeRoleService.getRoles(emp.email).pipe(first()).subscribe((data) => roles.push(...data));
+            return {
+              Name: emp.name + ' ' + emp.surname,
+              Position: emp.employeeType.name,
+              Level: emp.level,
+              Client: emp.clientAllocated ? emp.clientAllocated : 'Bench',
+              Roles: roles
+            }
+          })
+        }),
+        first()
+        )
+      .subscribe((data) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
+  }
+
+  reset(): void {
+    this.dataSource.filter = '';
+    this.dataSource._updateChangeSubscription();
   }
 
   CaptureEvent(event: any) {
-    const target = event.target as HTMLAnchorElement;
+    const target = event.target as HTMLButtonElement;
     this.cookieService.set('currentPage', target.innerText);
   }
 
-  ViewUser(email: string){
-    this.cookieService.set('selectedUser',email)
+  ViewUser(email: string) {
+    this.cookieService.set('selectedUser', email);
   }
 
-  employeeClickEvent(event: any, employee: EmployeeProfile): void{
-    this.selectedEmp = employee
-    this.selectedEmployee.emit(this.selectedEmp)
+  newEmployeeClickEvent(): void {
+    this.viewEmployee.emit(true);
+    this.cookieService.set('currentPage', 'New Employee');
+  }
+
+  employeeClickEvent(event: any, employee: EmployeeProfile): void {
+    this.selectedEmp = employee;
+    this.selectedEmployee.emit(this.selectedEmp);
     this.cookieService.set('currentPage', 'Employee Details');
   }
+
+  displayedColumns: string[] = ['Name', 'Position', 'Level', 'Client', 'Roles'];
+
+  dataSource: MatTableDataSource<{
+    Name: string;
+    Position: string;
+    Level: number;
+    Client: string;
+    Roles: string[];
+}> = new MatTableDataSource();
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  screenWidth: number = 992;
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  pageSizes: number[] = [1, 5, 10, 25, 100];
+
+  changePageSize(size: number) {
+    this.paginator.pageSize = size;
+    this.dataSource._updateChangeSubscription();
+  }
+
+  get pageIndex(): number {
+    return this.paginator.pageIndex;
+  }
+
+  prevPage() {
+    if (!this.paginator.hasPreviousPage()) return;
+    this.paginator.previousPage();
+  }
+
+  nextPage() {
+    if (!this.paginator.hasNextPage()) return;
+    this.paginator.nextPage();
+  }
+
+  get visiblePages(): number[] {
+    const totalPages = this.paginator.getNumberOfPages();
+    
+    let maxVisiblePages = this.screenWidth <= 992 ? 2 : 4;
+    
+    let startPage = Math.max(this.paginator.pageIndex - Math.floor(maxVisiblePages / 2), 0);
+    let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages - 1);
+    startPage = Math.max(endPage - maxVisiblePages + 1, 0);
+  
+    const pages: number[] = [];
+    for (let i = startPage; i <= endPage; i++) {
+      pages.push(i + 1);
+    }
+    return pages;
+  }  
 }
