ace2a4432ea7eba73852895e3eb86a5600e99b73
Lucien-vanWyk <123623749+Lucien-vanWyk@users.noreply.github.com>
2023-10-31 15:56:16 +0200

    Commit

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -6,6 +6,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
 import { EmployeeProfileService } from 'src/app/services/employee/employee-profile.service';
 import { EmployeeService } from 'src/app/services/employee/employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -20,7 +21,7 @@ export class AdminDashboardComponent {
   profileImage: string | null = null;
   initialDisplayCount: number = 3;
   displayAllEmployees: boolean = false;
-  roles : string[] = [];
+  roles: string[] = [];
 
   employeeType: { id: number; name: string } = {
     id: 0,
@@ -35,8 +36,9 @@ export class AdminDashboardComponent {
     private employeeProfileService: EmployeeProfileService,
     private employeeService: EmployeeService,
     private chartService: ChartService,
-    private cookieService: CookieService
-  ) { }
+    private cookieService: CookieService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const types: string = this.cookieService.get('userType');
@@ -48,23 +50,13 @@ export class AdminDashboardComponent {
         this.allEmployees = [data];
       }
       this.searchResults = [];
-
-      console.log(this.allEmployees);
-      
-
-      // if (
-      //   this.admin.photo &&
-      //   (this.admin.photo.startsWith('http') ||
-      //     this.admin.photo.startsWith('data:image'))
-      // ) {
-      //   this.profileImage = this.admin.photo;
-      // }
-      // this.employeeType = this.admin.employeeType;
     });
 
-    this.employeeProfileService.getAllEmployees(this.searchQuery).subscribe((data) => {
-      this.allEmployees = data;
-    });
+    this.employeeProfileService
+      .getAllEmployees(this.searchQuery)
+      .subscribe((data) => {
+        this.allEmployees = data;
+      });
 
     this.chartService.getAllCharts().subscribe({
       next: (data) => (this.charts = data),
@@ -86,7 +78,8 @@ export class AdminDashboardComponent {
 
   searchEmployees() {
     if (this.searchQuery) {
-      this.searchResults = this.allEmployees.filter(employee =>
+      this.searchResults = this.allEmployees.filter(
+        (employee) =>
           employee.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
           employee.surname.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
@@ -96,17 +89,20 @@ export class AdminDashboardComponent {
       this.deactivateSearchBar();
     }
   }
-  
+ 
   activateSearchBar() {
     const searchBar = document.querySelector('.searchbar');
     searchBar?.classList.add('active');
     searchBar?.classList.remove('no-results');
   }
-  
+
   deactivateSearchBar() {
     const searchBar = document.querySelector('.searchbar');
     searchBar?.classList.remove('active');
     searchBar?.classList.add('no-results');
   }
-  
+
+  navigateToEmployeeDetails(employeeId: number) {
+    this.router.navigate(['/employee', employeeId]);
+  }
 }
