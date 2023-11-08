dac0d2a5d974969833e507739c76bb84b7923bef
dvanhout89 <123621238+dvanhout89@users.noreply.github.com>
2023-11-08 12:22:26 +0200

    Update employee-document.service.ts
    Resolving comments on PR

--- src/app/services/employee/employee-document.service.ts
+++ src/app/services/employee/employee-document.service.ts
@@ -9,30 +9,20 @@ import { API } from '../../models/constants/urls.constants';
 })
 
 export class EmployeeDocumentService {
-
     constructor(private httpClient: HttpClient) { }
 
-    // Get all employee documents
     getAllEmployeeDocuments(employeeId: number): Observable<EmployeeDocument[]> {
         return this.httpClient.get<EmployeeDocument[]>(`${API.HttpsBaseURL}/employeedocument/getAll/${employeeId}`);
     }
-
-    // Save an employee document
     saveEmployeeDocument(employeeDocument: EmployeeDocument): Observable<EmployeeDocument> {
         return this.httpClient.post<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/save`, employeeDocument);
     }
-
-    // Get an employee document by employee ID and filename
     getEmployeeDocument(employeeId: number, filename: string): Observable<EmployeeDocument> {
         return this.httpClient.get<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/get/${employeeId}/${filename}`);
     }
-
-    // Update an employee document
     updateEmployeeDocument(employeeDocument: EmployeeDocument): Observable<EmployeeDocument> {
         return this.httpClient.put<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/update`, employeeDocument);
     }
-
-    // Delete an employee document
     deleteEmployeeDocument(employeeDocument: EmployeeDocument): Observable<void> {
         return this.httpClient.delete<void>(`${API.HttpsBaseURL}/employeedocument/delete/${employeeDocument.id}`);
     }
