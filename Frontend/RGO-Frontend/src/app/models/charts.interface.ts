dd00b24ccd90214d1348e41b5d5316dfc050203a
Lucien van Wyk <89403985+SpoopyBoii@users.noreply.github.com>
2023-11-08 13:27:52 +0200

    Removed old Front-End folder

--- Frontend/RGO-Frontend/src/app/models/charts.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Chart {
-    id: number;
-    name: string;
-    type: string;
-    dataType:string;
-    labels: string[];
-    data: number[];
-  }
--- Frontend/RGO-Frontend/src/app/models/constants/types.constants.ts
+++
@@ -1,7 +0,0 @@
-export const dataTypes = [
-    {id: 0, value: 'Date'},
-    {id: 1, value: 'String'},
-    {id: 2, value: 'Int'},
-    {id: 3, value: 'Bool'},
-    {id: 4, value: 'Options'}
-]
