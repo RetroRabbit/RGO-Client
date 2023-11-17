a1879c9086a355a2e39b826dc91102bd43ae82c2
Matthew Schoeman <mschoeman@retrorabbit.co.za>
2023-11-17 13:47:31 +0200

    Added diabling buttons

--- src/app/services/employee/employee-document.service.ts
+++ src/app/services/employee/employee-document.service.ts
@@ -14,14 +14,14 @@ export class EmployeeDocumentService {
     getAllEmployeeDocuments(employeeId: number): Observable<EmployeeDocument[]> {
         return this.httpClient.get<EmployeeDocument[]>(`${API.HttpsBaseURL}/employeedocument/all/?employeeId= ${employeeId}`);
     }
-    saveEmployeeDocument(employeeDocument: EmployeeDocument): Observable<EmployeeDocument> {
+    saveEmployeeDocument(employeeDocument: any): Observable<EmployeeDocument> {
+        console.log(employeeDocument);
         return this.httpClient.post<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/save`, employeeDocument);
     }
     getEmployeeDocument(employeeId: number, filename: string): Observable<EmployeeDocument> {
         return this.httpClient.get<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/get/${employeeId}/${filename}`);
     }
     updateEmployeeDocument(employeeDocument: EmployeeDocument): Observable<EmployeeDocument> {
-        console.log(employeeDocument);
         return this.httpClient.put<EmployeeDocument>(`${API.HttpsBaseURL}/employeedocument/update`, employeeDocument);
     }
     deleteEmployeeDocument(employeeDocument: EmployeeDocument): Observable<void> {
