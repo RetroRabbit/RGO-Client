77eaea2d6b57e23b09bc9dedeeb92cdf9f56b103
Dorothy Mahoko <mahokod@fourier.co.za>
2023-11-20 11:39:41 +0200

    Added some banks and proper input for bank

--- src/app/models/constants/banks.constants.ts
+++ src/app/models/constants/banks.constants.ts
@@ -3,5 +3,8 @@ export const banks = [
     {id:1, value: "Capitec"},
     {id:2, value: "First National Bank"},
     {id:3, value: "Nedbank"},
-    {id:4, value: "Standard Bank"}
+    {id:4, value: "Standard Bank"},
+    {id:4, value: "Thymebank"},
+    {id:4, value: "Discovery Bank"},
+    {id:4, value: "African Bank"}
   ]
