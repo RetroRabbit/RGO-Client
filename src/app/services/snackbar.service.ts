e9fc62b9d20da843644810ca92f4ab8107261c18
SuperSouls <gEsterhuyse@retrorabbit.co.za>
2023-11-27 14:43:46 +0200

    Added "X" button

--- src/app/services/snackbar.service.ts
+++ src/app/services/snackbar.service.ts
@@ -8,6 +8,6 @@ export class SnackbarService {
   constructor(public snackBar: MatSnackBar) {}
 
   showSnackbar(message: string, status: string) {
-    this.snackBar.open(message, "", {duration: 3000, panelClass: status, verticalPosition: 'top', horizontalPosition: 'center'})
+    this.snackBar.open(message, '✖', {duration: 3000, panelClass: status, verticalPosition: 'top', horizontalPosition: 'center'})
   }
 }
