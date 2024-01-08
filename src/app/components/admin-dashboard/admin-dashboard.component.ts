436847bf4ad5b16210e72065ab890648a0c4e926
Dorothy Mahoko <mahokod@fourier.co.za>
2024-01-08 12:00:32 +0200

    slight clean up and search on sign in bug fix

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,23 +1,24 @@
-import { Component, Output, EventEmitter, ViewChild, HostListener } from '@angular/core';
-import { Chart } from 'src/app/models/charts.interface';
-import { ChartService } from 'src/app/services/charts.service';
-import { CookieService } from 'ngx-cookie-service';
-import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
 import { EmployeeProfileService } from 'src/app/services/employee/employee-profile.service';
-import { EmployeeService } from 'src/app/services/employee/employee.service';
-import { Router } from '@angular/router';
-import { catchError, forkJoin, map, of, switchMap, tap } from 'rxjs';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
 import { EmployeeRoleService } from 'src/app/services/employee/employee-role.service';
-import { FormControl } from '@angular/forms';
+import { EmployeeTypeService } from 'src/app/services/employee/employee-type.service';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
+import { EmployeeProfile } from 'src/app/models/employee-profile.interface';
+import { Component, Output, EventEmitter, ViewChild } from '@angular/core';
+import { catchError, forkJoin, map, of, switchMap, tap } from 'rxjs';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { HideNavService } from 'src/app/services/hide-nav.service';
+import { EmployeeType } from 'src/app/models/employee-type.model';
+import { ChartService } from 'src/app/services/charts.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Chart } from 'src/app/models/charts.interface';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { MatDialog } from '@angular/material/dialog';
+import { CookieService } from 'ngx-cookie-service';
+import { MatSort } from '@angular/material/sort';
+import { FormControl } from '@angular/forms';
 import { TemplateRef } from '@angular/core';
-import { SnackbarService } from 'src/app/services/snackbar.service';
-import { EmployeeTypeService } from 'src/app/services/employee/employee-type.service';
-import { EmployeeType } from 'src/app/models/employee-type.model';
-import { HideNavService } from 'src/app/services/hide-nav.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
@@ -103,13 +104,6 @@ export class AdminDashboardComponent {
         }
         this.searchResults = [];
       }, complete: () => {
-        this.loadCounter++;
-      }
-    });
-
-    this.employeeProfileService.searchEmployees(this.searchQuery).subscribe({
-      next: data => this.allEmployees = data
-      , complete: () => {
         this.loadCounter++;
       }
     });
