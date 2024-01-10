18ed070adc64903adad2a677245fd55a55e995bc
Lerato Malinga <lmalinga@retrorabbit.co.za>
2024-01-10 16:13:52 +0200

    #6965-Discard Modal Functionality

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,13 +2,13 @@
 import { Component } from "@angular/core";
 import { AuthService } from "@auth0/auth0-angular";
 import { CookieService } from "ngx-cookie-service";
-import { HideNavService } from 'src/app/services/hide-nav.service';
+import { HideNavService } from 'src/app/services/hide-nav.service'; // use this service
 import { ChartService } from "src/app/services/charts.service";
 import { EmployeeProfileService } from "src/app/services/employee/employee-profile.service";
 import { Router } from '@angular/router';
 import { EmployeeProfile } from "src/app/models/employee-profile.interface";
 import { Chart } from "chart.js";
-
+import { Dialog } from 'src/app/models/confirm-modal.interface';
 
 @Component({
   selector: 'app-nav-bar',
@@ -16,7 +16,7 @@ import { Chart } from "chart.js";
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
-[x: string]: any;
+  [x: string]: any;
 
   title = 'HRIS';
   showNav = this.hideNavService.showNavbar;
@@ -31,14 +31,16 @@ export class NavBarComponent {
     name: ''
   };
 
+  showConfirmDialog: boolean = false;
+  dialogTypeData: Dialog = { type: '', title: '', subtitle: '', confirmButtonText: '', denyButtonText: '' };
+  tempRoute: string = '';
   constructor(
     private employeeProfileService: EmployeeProfileService,
     private chartService: ChartService,
     private auth: AuthService,
     public router: Router,
     public cookieService: CookieService,
-    public hideNavService: HideNavService)
-    {
+    public hideNavService: HideNavService) {
     this.screenWidth = window.innerWidth;
   }
 
@@ -46,7 +48,7 @@ export class NavBarComponent {
     this.signIn();
   }
 
-  signIn(){
+  signIn() {
     this.hideNavService.showNavbar = true;
     const types: string = this.cookieService.get('userType');
     const userEmail = this.cookieService.get('userEmail');
@@ -89,4 +91,26 @@ export class NavBarComponent {
       logoutParams: { returnTo: document.location.origin }
     });
   }
+  
+  changeNav(route: string) {
+
+    if (this.hideNavService.unsavedChanges) {
+      this.tempRoute = route;
+      this.dialogTypeData = { type: 'save', title: 'Discard unsaved changes', subtitle: '', confirmButtonText: 'DISCARD', denyButtonText: 'BACK' }
+
+      this.showConfirmDialog = true;
+    } else {
+      this.router.navigate([route]);
+    }
+  }
+
+  dialogFeedBack(event: any) {
+    this.showConfirmDialog = false;
+    if (event) {
+      this.hideNavService.unsavedChanges = false;
+      this.router.navigate([this.tempRoute]);
+    } else {
+    }
+  }
+
 }
