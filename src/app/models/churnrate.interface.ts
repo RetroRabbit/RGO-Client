8c419803ad4cca7758127b982ddcf5b7cb833919
Lerato Malinga <lmalinga@retrorabbit.co.za>
2024-01-18 14:54:22 +0200

    #7276-Updated churnrate interface and datacard

--- src/app/models/churnrate.interface.ts
+++ src/app/models/churnrate.interface.ts
@@ -1,5 +1,9 @@
 export interface ChurnRate {
   churnRate: number;
+  developerChurnRate: number,
+  designerChurnRate: number,
+  scrumMasterChurnRate: number,
+  businessSupportChurnRate: number,
   month: string;
   year: number;
 }
