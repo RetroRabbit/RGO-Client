c828e6fec71702e4923b863be2599affc49ca9a4
Dorothy Mahoko <mahokod@fourier.co.za>
2024-02-13 22:43:06 +0200

    clean up on admin dasboard ts

--- src/app/services/employee/employee.service.ts
+++ src/app/services/employee/employee.service.ts
@@ -14,35 +14,36 @@ import { EmployeeCountDataCard } from 'src/app/models/employee-count-data-card.i
 })
 export class EmployeeService {
   baseUrl: string;
-  
+
   constructor(private httpClient: HttpClient,
-    private authAccessService: AuthAccessService) { 
-    this.baseUrl =`${API.HttpsBaseURL}/employees`
+    private authAccessService: AuthAccessService) {
+    this.baseUrl = `${API.HttpsBaseURL}/employees`
   }
 
-   /**
- * @summary  Gets the Basic Employee information ie.  
- *  id
- *  name
- *  surname
- *  email
- *  joinDate
- *
- * @returns List of Basic Employee objects.
- */
-  getAll(): Observable<Employee[]>{
+  /**
+* @summary  Gets the Basic Employee information ie.  
+*  id
+*  name
+*  surname
+*  email
+*  joinDate
+*
+* @returns List of Basic Employee objects.
+*/
+  getAll(): Observable<Employee[]> {
     return this.httpClient.get<Employee[]>(`${this.baseUrl}/all`);
   }
+
   /**
  * @summary  Gets all the Full Employee DTO and information
  *
- * @returns List of Employee DTO objects.
+ * @returns Array of EmployeeProfile objects.
  */
-  getAllProfiles(): Observable<EmployeeProfile[]>{
+  getEmployeeProfiles(): Observable<EmployeeProfile[]> {
     return this.httpClient.get<EmployeeProfile[]>(`${this.baseUrl}/all`);
   }
 
-  addEmployee(newEmployee: any): Observable<any>{
+  addEmployee(newEmployee: any): Observable<any> {
     return this.httpClient.post<any>(`${this.baseUrl}`, newEmployee);
   }
 
@@ -50,7 +51,7 @@ export class EmployeeService {
     return this.httpClient.get<EmployeeProfile>(`${this.baseUrl}/by-email?email=${encodeURIComponent(email)}`);
   }
 
-  updateEmployee(employee: any): Observable<any>{
+  updateEmployee(employee: any): Observable<any> {
     const queryParams = `?userEmail=${this.authAccessService.getEmployeeEmail()}`
     return this.httpClient.put<any>(`${this.baseUrl}/update/${queryParams}`, employee)
   }
@@ -67,7 +68,7 @@ export class EmployeeService {
     return this.httpClient.get<ChurnRateDataCard>(`${this.baseUrl}/churn-rate`);
   }
 
-  getDevsDesignerCount(): Observable<EmployeeCountDataCard> {
+  getEmployeeCountData(): Observable<EmployeeCountDataCard> {
     return this.httpClient.get<EmployeeCountDataCard>(`${this.baseUrl}/card-count`);
   }
 }
