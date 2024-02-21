77602635623ef9fef494a48bd5b68c4ab2bb4ae5
Emmanuel <Emmanuelmuya@gmail.com>
2024-02-21 08:10:41 +0200

    Update environment.ts

--- src/enviroment/env.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  Security_Audience: 'Https://Abstract_layer_api/', //create a api gateway in auth0
-  Main_redirect_URL: 'http://localhost:4200/',
-  Abstract_Layer_URL: 'https://529b-105-186-112-70.ap.ngrok.io/api/', // comunicate to backend
-};
