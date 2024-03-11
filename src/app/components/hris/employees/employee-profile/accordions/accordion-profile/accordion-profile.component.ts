135a480bd50ba5f555f8c73f9bc2bcff26ecc5d4
AWallsRR <awalls@retrorabbit.co.za>
2024-03-11 10:15:00 +0200

    remove uneeded classes

--- src/app/components/hris/employees/employee-profile/accordions/accordion-profile/accordion-profile.component.ts
+++ src/app/components/hris/employees/employee-profile/accordions/accordion-profile/accordion-profile.component.ts
@@ -151,59 +151,6 @@ export class AccordionProfileComponent {
     postalPostalCode: { value: '', disabled: true }
   });
 
-  employeeProfilePermissions: EmployeeProfilePermissions = {
-    id: true,
-    employeeNumber: true,
-    taxNumber: true,
-    engagementDate: true,
-    terminationDate: true,
-    peopleChampion: true,
-    disability: true,
-    disabilityNotes: true,
-    countryOfBirth: true,
-    nationality: true,
-    level: true,
-    employeeType: true,
-    name: true,
-    initials: true,
-    surname: true,
-    dateOfBirth: true,
-    idNumber: true,
-    passportNumber: true,
-    passportExpirationDate: true,
-    passportCountryIssue: true,
-    race: true,
-    gender: true,
-    email: true,
-    personalEmail: true,
-    cellphoneNo: true,
-    photo: true,
-    notes: true,
-    leaveInterval: true,
-    salaryDays: true,
-    payRate: true,
-    salary: true,
-    clientAllocated: true,
-    teamLead: true,
-    physicalAddress: true,
-    postalAddress: true,
-    houseNo: true,
-    emergencyContactName: true,
-    emergencyContactNo: true
-  };
-
-  employeeAddressPermissions: EmployeeAddressPermissions = {
-    id: true,
-    unitNumber: true,
-    complexName: true,
-    streetNumber: true,
-    suburbOrDistrict: true,
-    city: true,
-    country: true,
-    province: true,
-    postalCode: true
-  }
-
   additionalInfoForm: FormGroup = this.fb.group({});
   constructor(
     private fb: FormBuilder,
