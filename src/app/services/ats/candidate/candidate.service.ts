417fce27ed32c7cdc8031114ddd60fafedd493df
Juanro-Minne <jminne@retrorabbit.co.za>
2024-03-11 12:01:22 +0200

    added service file

---
+++ src/app/services/ats/candidate/candidate.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../../../models/hris/employee.interface';
+import { EmployeeProfile } from '../../../models/hris/employee-profile.interface';
+import { environment } from '../../../../environments/environment';
+import { AuthAccessService } from '../../shared-services/auth-access/auth-access.service';
+
+@Injectable({
+    providedIn: 'root'
+  })
+
+export class CandidateService {
+    baseUrl: string;
+
+  constructor(private httpClient: HttpClient,
+    private authAccessService: AuthAccessService) {
+    this.baseUrl = `${environment.HttpsBaseURL}/employees`
+  }
+}
