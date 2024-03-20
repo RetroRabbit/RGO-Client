94ac80a57e4123edee3ba4abc532aee4031e8415
Reino Powell <rpowell@retrorabbit.co.za>
2024-03-20 13:57:49 +0200

    Added new component

---
+++ src/app/components/hris/employees/employee-profile/accordions/accordion-profile/accordion-profile-personal-details/accordion-profile-personal-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-profile-personal-details',
+  templateUrl: './accordion-profile-personal-details.component.html',
+  styleUrls: ['./accordion-profile-personal-details.component.css']
+})
+export class AccordionProfilePersonalDetailsComponent {
+
+}
