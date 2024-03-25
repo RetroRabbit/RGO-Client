407e628364a81a2afaf77de4f526f207d683b3ac
Emmanuel <ENsanga@retrorabbit.co.za>
2024-03-25 09:56:32 +0200

    Update hris-guard.guard.ts

--- src/app/hris-guard.guard.ts
+++ src/app/hris-guard.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivateFn} from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { NavService } from './services/shared-services/nav-service/nav.service';
 import { AuthService } from './services/shared-services/auth-access/auth.service';
@@ -9,19 +9,20 @@ import { environment } from 'src/environments/environment';
 })
 
 export class HrisPageGuard {
-  constructor( private navService: NavService,
+  constructor(private navService: NavService,
     private authService: AuthService
-    ) {}
+  ) { }
 
   canActivate: CanActivateFn = () => {
     if (this.navService.isHris) {
       return true;
     }
 
-    if(this.navService.isHris == undefined){
+    if (this.navService.isHris = environment.development && this.navService.isHris == undefined) {
       this.navService.isHris = environment.development;
+    } else {
       this.authService.logout()
-   }
+    }
     return false;
   };
 }
