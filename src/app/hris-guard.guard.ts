238685a1c0c174d4117ff5c07e64b83e4a5f9083
Emmanuel <ENsanga@retrorabbit.co.za>
2024-03-26 14:19:57 +0200

    Resetting isHris variable , refetching user roles in HRIS Guard

--- src/app/hris-guard.guard.ts
+++ src/app/hris-guard.guard.ts
@@ -1,9 +1,12 @@
-import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { NavService } from './services/shared-services/nav-service/nav.service';
 import { AuthService } from './services/shared-services/auth-access/auth.service';
 import { AuthGuard } from '@auth0/auth0-angular';
 import { environment } from 'src/environments/environment';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthAccessService } from './services/shared-services/auth-access/auth-access.service';
+import { map, take, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +15,9 @@ export class HrisPageGuard {
   constructor(private navService: NavService,
     private authService: AuthService,
     private hrisGuard: AuthGuard,
+    private cookieService: CookieService,
+    private router: Router,
+    private authAccessService : AuthAccessService
   ) { }
 
   canActivate(_route: ActivatedRouteSnapshot, _state:
@@ -19,13 +25,18 @@ export class HrisPageGuard {
     if (this.navService.isHris) {
       return true;
     }
-    if (this.navService.isHris == undefined && environment.development) {
-      this.navService.isHris = environment.development
-      return this.hrisGuard.canActivate(_route, _state);
-    }
-    else if (this.navService.isHris == undefined) {
+
+    if (this.navService.isHris == undefined) {
+      if (environment.development) {
+        this.navService.isHris = Boolean(this.cookieService.get('isHris'));
+        this.authService.FetchRoles(this.cookieService.get('userEmail')).subscribe({
+          next: roles => {
+            this.authAccessService.setRoles(roles);
+          }
+        });
+        return true;
+      } 
       this.authService.logout()
     }
-    return false;
   }
 }
