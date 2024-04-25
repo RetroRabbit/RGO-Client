e2111ba6216c4652653d254370ee058b24d28eb7
Reino Powell <rpowell@retrorabbit.co.za>
2024-04-25 14:54:55 +0200

    Revert "Removed unused line"
    This reverts commit 604ceaf1121a801ab57c111bf002566e495bef91.

--- src/app/components/hris/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/hris/admin-dashboard/admin-dashboard.component.ts
@@ -199,6 +199,7 @@ export class AdminDashboardComponent {
     this.chartService.getAllCharts().subscribe({
       next: (data) => {
         this.charts = data;
+        this.charts[0].data;
       },
       error: () => {
         this.snackBarService.showSnackbar(
