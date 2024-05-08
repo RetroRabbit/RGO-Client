773089c8fb8227ce6510704af52cb19235d31637
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-05-08 10:36:06 +0200

    Added custom fileds func  to CustomDocuments ts file

--- src/app/components/hris/employees/employee-profile/accordions-documents/accordion-documents-custom-documents/accordion-documents-custom-documents.component.ts
+++ src/app/components/hris/employees/employee-profile/accordions-documents/accordion-documents-custom-documents/accordion-documents-custom-documents.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CustomField } from 'src/app/models/hris/custom-field.interface';
+import { EmployeeData } from 'src/app/models/hris/employee-data.interface';
+import { CustomFieldService } from 'src/app/services/hris/field-code.service';
+import { SharedAccordionFunctionality } from '../../shared-accordion-functionality';
 
 @Component({
   selector: 'app-accordion-documents-custom-documents',
@@ -7,4 +12,43 @@ import { Component } from '@angular/core';
 })
 export class AccordionDocumentsCustomDocumentsComponent {
 
+  // customFields: CustomField[] = [];
+
+  // constructor(
+  //   private customFieldService: CustomFieldService,
+  //   private fb: FormBuilder,
+  //   public sharedAccordionFunctionality: SharedAccordionFunctionality) { }
+
+  // getDocumentsFieldCodes() {
+  //   this.customFieldService.getAllFieldCodes().subscribe({
+  //     next: data => {
+  //       this.customFields = data.filter((data: CustomField) => data.category === this.sharedAccordionFunctionality.category[0].id);
+  //       this.checkCustomDocumentsInformation();
+  //     }
+  //   })
+  // }
+
+  // checkCustomDocumentsInformation() {
+  //   const formGroupConfig: any = {};
+  //   this.customFields.forEach(fieldName => {
+  //     if (fieldName.code != null || fieldName.code != undefined) {
+  //       const customData = this.sharedAccordionFunctionality.employeeData.filter((data: EmployeeData) => data.fieldCodeId === fieldName.id)
+  //       formGroupConfig[fieldName.code] = new FormControl({ value: customData[0] ? customData[0].value : '', disabled: true });
+  //       this.sharedAccordionFunctionality.additionalInfoForm = this.fb.group(formGroupConfig);
+  //       if (fieldName.required == true) {
+  //         this.sharedAccordionFunctionality.additionalInfoForm.controls[fieldName.code].setValidators(Validators.required);
+  //       }
+  //       this.sharedAccordionFunctionality.additionalInfoForm.disable();
+  //     }
+  //   });
+  // }
+  // getDocumentFieldCodes() {
+  //   this.customFieldService.getAllFieldCodes().subscribe({
+  //     next: data => {
+  //       this.customFields = data.filter((data: CustomField) => data.category === this.sharedAccordionFunctionality.category[0].id);
+  //       this.checkAdditionalInformation();
+
+  //     }
+  //   })
+  // }
 }
