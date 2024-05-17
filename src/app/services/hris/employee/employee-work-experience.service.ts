f7f0a8a9b822d8933e2e710dfb9b9e9d1c9c110a
Reino Powell <rpowell@retrorabbit.co.za>
2024-05-17 09:20:48 +0200

    Created service for the accordion

---
+++ src/app/services/hris/employee/employee-work-experience.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { WorkExperience } from 'src/app/models/hris/work-experience.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkExperienceService {
+
+    baseUrl: string;
+    
+    constructor(private httpClient: HttpClient) { 
+        this.baseUrl =`${environment.HttpsBaseURL}/work-experience`
+    }
+
+    // GetWorkExperience(): Observable<WorkExperience> {
+    //     return this.httpClient.get<WorkExperience>(`${environment.HttpsBaseURL}/work-experience`);
+    // }
+
+    getWorkExperience(id: number | undefined): Observable<WorkExperience> {
+        return this.httpClient.get<WorkExperience>(`${this.baseUrl}/?id=${id}`)
+      }
+
+    save(experience: WorkExperience): Observable<WorkExperience> {
+        return this.httpClient.post<WorkExperience>(`${this.baseUrl}`, experience);
+    }
+
+    update(experience: WorkExperience): Observable<WorkExperience> {
+        return this.httpClient.put<WorkExperience>(`${this.baseUrl}`, experience);
+    }
+
+    delete(experienceId: WorkExperience): Observable<WorkExperience> {
+        return this.httpClient.delete<WorkExperience>(`${this.baseUrl}/${experienceId}`);
+    }
+}
