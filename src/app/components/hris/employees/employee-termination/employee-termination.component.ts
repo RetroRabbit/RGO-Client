a7ea0f5759b057b8939128cc8424cae88d0d49e6
Juanro-Minne <jminne@retrorabbit.co.za>
2024-06-04 10:12:58 +0200

    error states

--- src/app/components/hris/employees/employee-termination/employee-termination.component.ts
+++ src/app/components/hris/employees/employee-termination/employee-termination.component.ts
@@ -1,8 +1,9 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { terminationOptions } from 'src/app/models/hris/constants/terminationOptions.constants';
 import { NavService } from 'src/app/services/shared-services/nav-service/nav.service';
 import { Router } from '@angular/router';
+import { SnackbarService } from 'src/app/services/shared-services/snackbar-service/snackbar.service';
 
 @Component({
   selector: 'app-employee-termination',
@@ -10,22 +11,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./employee-termination.component.css']
 })
 export class EmployeeTerminationComponent implements OnInit {
-  newterminationform!: FormGroup;
-  terminationOption: string[] = terminationOptions.map((termination) => termination.value);
-  isMobileScreen = false;
-  screenWidth = window.innerWidth;
-  interviewFilename: string = "";
-
   @HostListener('window:resize', ['$event'])
   onResize() {
     this.isMobileScreen = window.innerWidth < 768;
     this.screenWidth = window.innerWidth;
   }
 
+  newterminationform!: FormGroup;
+  terminationOption: string[] = terminationOptions.map((termination) => termination.value);
+  isMobileScreen = false;
+  selectedFile !: File;
+  screenWidth = window.innerWidth;
+  interviewFilename: string = "";
+
   constructor(
     private navService: NavService,
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private snackBarService : SnackbarService
   ) { }
 
   ngOnInit() {
@@ -35,23 +38,16 @@ export class EmployeeTerminationComponent implements OnInit {
 
   initializeForm() {
     this.newterminationform = this.fb.group({
-      terminationOption: new FormControl(''),
-      dateOfNotice: new FormControl<Date | null>(null),
-      lastDayOfEmployment: new FormControl<Date | null>(null),
-      reEmployment: new FormControl<boolean>(true),
-      offBoardingTasks: new FormControl<boolean>(false),
-      exitInterviewDoc: new FormControl<boolean>(false),
+      terminationOption: new FormControl('', Validators.required),
+      dateOfNotice: new FormControl<Date | null>(null, Validators.required),
+      lastDayOfEmployment: new FormControl<Date | null>(null, Validators.required),
+      reEmployment: new FormControl<boolean>(true, Validators.required),
+      offBoardingTasks: new FormControl<boolean>(false, Validators.required),
+      exitInterviewDoc: new FormControl<boolean>(false, Validators.required),
       additionalComments: new FormControl<string>(''),
     });
   }
-
-  disableDownload(){
-
-  }
-
-  downloadDocument() {
-
-  }
+  
 
   removeDocument() {
     this.interviewFilename = '';
@@ -62,19 +58,11 @@ export class EmployeeTerminationComponent implements OnInit {
   }
 
   onFileChange(){
-    
+
   }
 
   uploadDocument(event: any) {
-    // this.isLoadingUpload = true;
-    // this.selectedFile = event.target.files[0];
-    // this.documentsFileName = this.selectedFile.name;
-    // if (this.allowedTypes.includes(this.selectedFile.type)) {
-    //   this.uploadProfileDocument();
-    // } else {
-    //   this.snackBarService.showSnackbar("Please upload a PDF", "snack-error");
-    //   this.isLoadingUpload = false;
-    // }
+    
   }
 
   goToPreviousPage() {
