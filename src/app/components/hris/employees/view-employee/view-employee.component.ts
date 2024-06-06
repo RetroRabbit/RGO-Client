b9096f31a4595c8b22ed52b75be8e8bc8c87d096
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-06-06 09:41:23 +0200

    whitespace removed

--- src/app/components/hris/employees/view-employee/view-employee.component.ts
+++ src/app/components/hris/employees/view-employee/view-employee.component.ts
@@ -247,7 +247,6 @@ export class ViewEmployeeComponent {
     const year = newDate.getFullYear();
     const month = String(newDate.getMonth() + 1).padStart(2, '0');
     const day = String(newDate.getDate()).padStart(2, '0');
-
     return `${day}/${month}/${year}`
   }
 
