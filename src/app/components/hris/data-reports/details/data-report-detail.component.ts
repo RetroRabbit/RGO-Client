28f05b4efaca6287939bba696c58b71bc848c771
EstiaanBritz <156115509+EstiaanBritz@users.noreply.github.com>
2024-06-19 15:53:06 +0200

    fixed functionality

--- src/app/components/hris/data-reports/details/data-report-detail.component.ts
+++ src/app/components/hris/data-reports/details/data-report-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, Output } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataReport } from 'src/app/models/hris/data-report.interface';
 import { DataReportColumns } from 'src/app/models/hris/data-report-columns.interface';
@@ -6,88 +6,104 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../../environments/environment';
 import { CookieService } from 'ngx-cookie-service';
+import { SnackbarService } from 'src/app/services/shared-services/snackbar-service/snackbar.service';
 
 @Component({
-    selector: 'app-data-report-detail',
-    templateUrl: './data-report-detail.component.html',
-    styleUrls: ['./data-report-detail.component.css']
+  selector: 'app-data-report-detail',
+  templateUrl: './data-report-detail.component.html',
+  styleUrls: ['./data-report-detail.component.css']
 })
+
 export class DataReportDetailComponent {
-    
-    baseUrl: string;
-    dataReportCode: string;
 
-    constructor(private router: Router,
-      private httpClient: HttpClient,
-      private cookieService: CookieService,
-      private route: ActivatedRoute) {
-        this.baseUrl = `${environment.HttpsBaseURL}/data-reports`
-        this.dataReportCode = this.route.snapshot.params["code"]
+  baseUrl: string;
+  dataReportCode: string;
+  isLoading: boolean = false;
+  dataObjects: DataReport = {};
+  screenWidth = window.innerWidth;
+  PREVIOUS_PAGE = 'previousPage';
+
+  constructor(private router: Router,
+    private httpClient: HttpClient,
+    private cookieService: CookieService,
+    private route: ActivatedRoute,
+    private snackBarService: SnackbarService) {
+    this.baseUrl = `${environment.HttpsBaseURL}/data-reports`
+    this.dataReportCode = this.route.snapshot.params["code"]
+  }
+
+  @HostListener('window:resize', ['$event'])
+
+  onResize() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.onResize();
+    this.dataReportCode = this.route.snapshot.params["id"]
+    this.populateReportData(this.dataReportCode);
+  }
+
+  fetchReportData(reportCode: string): Observable<DataReport> {
+    return this.httpClient.get<DataReport>(`${this.baseUrl}/get-data-report?code=${reportCode}`);
+  }
+
+  updateReportData(reportInput: any): Observable<any> {
+    return this.httpClient.post<any>(`${this.baseUrl}/update-report-input`, reportInput)
+  }
+
+  populateReportData(dataReportCode: string) {
+    this.fetchReportData(dataReportCode).subscribe({
+      next: data => {
+        this.dataObjects.reportId = data.reportId;
+        this.dataObjects.reportName = data.reportName;
+        this.dataObjects.columns = data.columns;
+        this.dataObjects.data = data.data;
+        this.isLoading = false
+      },
+    })
+  }
+
+  onViewEmployee(employeeId: number): void {
+    this.router.navigateByUrl('/profile/' + employeeId);
+    this.cookieService.set(this.PREVIOUS_PAGE, '/data-reports/' + this.dataReportCode);
+  }
+
+  getProperty(obj: any, key: string): any {
+    return obj[key];
+  }
+
+  UpdateCustomInput(index: number, column: DataReportColumns, e: any) {
+    var reportId = this.dataObjects.reportId
+    var columnId = column.id;
+    var employeeId = this.dataObjects.data![index].Id;
+    var checkBoxChecked = e.srcElement.checked;
+    var input = e.srcElement.value;
+
+    if (checkBoxChecked == true) {
+      input = "true";
+    }
+    if (checkBoxChecked == false) {
+      input = "false"
     }
 
-    dataObjects : DataReport = {}; // TODO : Pull this from back-end -> /data-reports/get-data-report?code=reportCode
+    this.dataObjects.data![index][column.prop] = input;
 
-    screenWidth = window.innerWidth;
-    PREVIOUS_PAGE = 'previousPage';
-
-    @HostListener('window:resize', ['$event'])
-    
-    onResize() {
-      this.screenWidth = window.innerWidth;
+    var reportInput = {
+      reportId: reportId,
+      columnId: columnId,
+      employeeId: employeeId,
+      input: input
     }
 
-    ngOnInit() {
-      this.onResize();
-      this.dataReportCode = this.route.snapshot.params["id"]
-      this.populateReportData(this.dataReportCode);
-    }
-
-    fetchReportData(reportCode: string): Observable<DataReport>{
-      return this.httpClient.get<DataReport>(`${this.baseUrl}/get-data-report?code=${reportCode}`);
-    }
-
-    populateReportData(dataReportCode: string){
-      this.fetchReportData(dataReportCode).subscribe({
-        next: data => {
-          this.dataObjects.reportId = data.reportId;
-          this.dataObjects.reportName = data.reportName;
-          this.dataObjects.columns = data.columns;
-          this.dataObjects.data = data.data;
-        }
-      })
-    }
-
-    onViewEmployee(employeeId: number): void {
-      this.router.navigateByUrl('/profile/' + employeeId);
-      this.cookieService.set(this.PREVIOUS_PAGE, '/data-reports/'+ this.dataReportCode);
-
-      //TODO : Set return path so user can navigate back to table view from employee view
-    }
-
-    getProperty(obj: any, key: string): any {
-      return obj[key];
-    }
-
-    
-    UpdateCustomInput(index: number, column: DataReportColumns, e: any) {
-      var reportId = this.dataObjects.reportId
-      var columnId = column.id;
-      var employeeId = this.dataObjects.data![index].Id;
-      var input = e.srcElement.value;
-
-      this.dataObjects.data![index][column.prop] = input;
-      
-      //TODO : submit change to BE & provide popup to notify changes has been saved
-
-      /*
-      URL: /data-reports/update-report-input
-      METHOD: POST
-      BODY: {
-              ReportId
-              ColumnId
-              EmployeeId
-              Input
-            }
-      */
-    }
+    this.updateReportData(reportInput).subscribe({
+      next: data => {
+        this.snackBarService.showSnackbar("Report Updated", "snack-success")
+      },
+      error: error => {
+        this.snackBarService.showSnackbar("Report Update Failed", "snack-error")
+      }
+    })
+  }
 }
