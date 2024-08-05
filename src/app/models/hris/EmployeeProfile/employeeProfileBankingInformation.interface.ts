fa23cfda472cbbd2a56e31a0ced45aa4d5e099f0
Juanro-Minne <jminne@retrorabbit.co.za>
2024-08-05 15:48:30 +0200

    removed field

--- src/app/models/hris/EmployeeProfile/employeeProfileBankingInformation.interface.ts
+++ src/app/models/hris/EmployeeProfile/employeeProfileBankingInformation.interface.ts
@@ -1,9 +1,7 @@
 import { EmployeeBanking } from "../employee-banking.interface";
 import { EmployeeData } from "../employee-data.interface";
-import { EmployeeProfileSalary } from "./employeeProfileSalaryDetails.interface";
 
 export interface employeeProfileBanking {
-    employeeSalary : EmployeeProfileSalary,
     employeeBanking : EmployeeBanking,
     employeedata : EmployeeData
 }
