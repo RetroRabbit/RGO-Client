5cf5d02c64ec306c4dde349b00974c06b30ad822
bseptember <bseptember@retrorabbit.co.za>
2024-08-06 17:40:51 +0200

    fix: removed imports

--- src/app/components/shared-components/store/app.state.ts
+++ src/app/components/shared-components/store/app.state.ts
@@ -1,6 +1,5 @@
 import { ClientState } from "./reducers/client.reducer";
 import { CustomFieldState } from "./reducers/custom-field.reducer";
-import { EmployeeTypesState } from "./reducers/employee-types.reducer";
 import { TokenState } from "./reducers/sign-in.reducer";
 
 export interface AppState {
--- src/app/services/shared-services/store-service/store-access.service.ts
+++ src/app/services/shared-services/store-service/store-access.service.ts
@@ -3,7 +3,6 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/components/shared-components/store/app.state';
 import { selectClients } from 'src/app/components/shared-components/store/selector/client.selector';
 import { selectCustomField } from 'src/app/components/shared-components/store/selector/custom-field.selector';
-import { selectEmployeeTypes } from 'src/app/components/shared-components/store/selector/employee-types.selector';
 import { Client } from 'src/app/models/hris/client.interface';
 import { CustomField } from 'src/app/models/hris/custom-field.interface';
 import { EmployeeType } from 'src/app/models/hris/employee-type.model';
