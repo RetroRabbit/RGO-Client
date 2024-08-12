c5fa1f3c6517288356945713910bc810184f0f16
Ntuthuko Zikalala <nzikalala@retroreabbit.co.za>
2024-08-12 09:45:21 +0200

    removed comments

--- src/app/models/hris/data-report.interface.ts
+++ src/app/models/hris/data-report.interface.ts
@@ -3,7 +3,7 @@ import { DataReportColumns } from "./data-report-columns.interface";
 import { DataReportFilter } from "./data-report-filter.interface";
 
 export interface DataReport {
-    // reportName?: string,
+    reportName?: string,
     reportId?: number,
     columns?: DataReportColumns[],
     data?: any[],
