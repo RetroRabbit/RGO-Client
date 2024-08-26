b9f7e4470999f25b02e85f359714220195bae95b
Reino Powell <rpowell@retrorabbit.co.za>
2024-08-26 15:29:04 +0200

    Changed shared accordion functionality type back to string

--- src/app/components/hris/employees/employee-profile/shared-accordion-functionality.ts
+++ src/app/components/hris/employees/employee-profile/shared-accordion-functionality.ts
@@ -81,7 +81,7 @@ export class SharedAccordionFunctionality {
     fieldOfStudy: "",
     year: "",
     nqfLevel: nqfLevels,
-    proofOfQualification: new ArrayBuffer(0),
+    proofOfQualification: "",
     documentName: "",
   };
 
