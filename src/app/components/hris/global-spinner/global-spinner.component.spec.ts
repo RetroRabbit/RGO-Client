91810bebd7f17c158d04110138318c8be60cb2bf
MathildaShongwe15 <156072510+MathildaShongwe15@users.noreply.github.com>
2024-08-27 08:54:04 +0200

    Update global-spinner.component.spec.ts

--- src/app/components/hris/global-spinner/global-spinner.component.spec.ts
+++ src/app/components/hris/global-spinner/global-spinner.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GlobalSpinnerComponent } from './global-spinner.component';
 
 describe('GlobalSpinnerComponent', () => {
