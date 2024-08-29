bb0de04ea626d6ba9a8d72c15dcba1b7e2c91ad5
Reino Powell <rpowell@retrorabbit.co.za>
2024-08-29 12:30:00 +0200

    Added more methods to file processing service and removed the comments

--- src/app/services/hris/file-processing.service.ts
+++ src/app/services/hris/file-processing.service.ts
@@ -21,16 +21,6 @@ export class FileProcessingService {
     const decompressedData = pako.ungzip(compressedData); // Gzip decompress
     return decompressedData.buffer;
   }
-
-  // deserializeAndDecompressFile(file: any) {
-  //   if (!file || !file.buffer) {
-  //     console.error("File or buffer is undefined.");
-  //     return;
-  //   }
-  //   // Rest of your method logic
-  //   const decompressedData = pako.ungzip(file); // Gzip decompress
-  //   return decompressedData.buffer;
-  // }
   
   // Convert a File object to ArrayBuffer (synchronously using FileReader)
   fileToArrayBuffer(file: File, callback: (arrayBuffer: ArrayBuffer) => void): void {
@@ -53,26 +43,35 @@ export class FileProcessingService {
 
   // converting compressed array buffer to base64 string for sending to backend
 
-  arrayBufferToBase64(buffer: ArrayBuffer): string {
-    let binary = '';
-    const bytes = new Uint8Array(buffer);
-    const len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
-  }
-
   stringToByteArray(docString: string): Uint8Array {
     const encoder = new TextEncoder();
     const byteArray =  encoder.encode(docString);
     return byteArray;
   }
 
-  byteArrayToString(byteArray: ArrayBuffer): string {
+  byteArrayToString(byteArray: Uint8Array): string {
     const decoder = new TextDecoder();
     const newString =  decoder.decode(byteArray)
     return newString;
   }
+
+  convertUint8ArrayToBase64(uint8Array: Uint8Array): string {
+    let binaryString = "";
+   for (let i = 0; i < uint8Array.length; i++) {
+     binaryString += String.fromCharCode(uint8Array[i]);
+   }
+   return btoa(binaryString);
+ }
+ 
+ convertBase64ToUint8Array(base64String: string): Uint8Array {
+   let binaryString = atob(base64String);
+   let length = binaryString.length;
+   let uint8Array = new Uint8Array(length);
+ 
+   for (let i = 0; i < length; i++) {
+     uint8Array[i] = binaryString.charCodeAt(i);
+   }
+   return uint8Array;
+ }
 
 }
