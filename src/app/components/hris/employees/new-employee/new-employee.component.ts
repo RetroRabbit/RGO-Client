75091b08f3f20bf43340481e8aa784166ffb7744
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-08-30 11:04:50 +0200

    removed postal address from  form

--- src/app/components/hris/employees/new-employee/new-employee.component.ts
+++ src/app/components/hris/employees/new-employee/new-employee.component.ts
@@ -108,7 +108,6 @@ export class NewEmployeeComponent implements OnInit {
   };
 
   physicalAddress: FormGroup = this.createAddressForm();
-  postalAddress: FormGroup = this.createAddressForm();
   newEmployeeForm: FormGroup = this.createEmployeeForm();
 
   settingsForm: FormGroup = new FormGroup({
@@ -116,10 +115,6 @@ export class NewEmployeeComponent implements OnInit {
       false,
       Validators.required
     ),
-  });
-
-  postalAddressForm: FormGroup = new FormGroup({
-    sameAsPhysicalAddress: new FormControl<boolean>(true, Validators.required),
   });
 
   uploadDocumentForm = new FormGroup({
@@ -209,7 +204,6 @@ export class NewEmployeeComponent implements OnInit {
     this.clearFormErrorsAndValues(this.newEmployeeForm);
     this.clearFormErrorsAndValues(this.uploadDocumentForm);
     this.clearFormErrorsAndValues(this.physicalAddress);
-    this.clearFormErrorsAndValues(this.postalAddressForm);
     this.removeAllDocuments();
   }
 
@@ -220,7 +214,6 @@ export class NewEmployeeComponent implements OnInit {
     this.clearFormErrorsAndValues(this.newEmployeeForm);
     this.clearFormErrorsAndValues(this.uploadDocumentForm);
     this.clearFormErrorsAndValues(this.physicalAddress);
-    this.clearFormErrorsAndValues(this.postalAddressForm);
     this.removeAllDocuments();
     this.newEmployeeForm.controls['engagementDate'].setValue(new Date(Date.now()));
     this.newEmployeeForm.controls['disability'].setValue(false);
@@ -294,39 +287,6 @@ export class NewEmployeeComponent implements OnInit {
       this.inputField.reset();
       document.querySelector('.cellphone-container')?.classList.remove('has-value');
     }
-  }
-
-  postalSameAsPhysicalAddress(event: boolean) {
-    this.isSameAddress = event;
-
-    if (this.postalAddressForm.value.sameAsPhysicalAddress && event) {
-      this.postalAddress.patchValue({
-        unitNumber: this.physicalAddress.value.unitNumber,
-        complexName: this.physicalAddress.value.complexName,
-        suburbDistrict: this.physicalAddress.value.suburbDistrict,
-        city: this.physicalAddress.value.city,
-        streetNumber: this.physicalAddress.value.streetNumber,
-        streetName: this.physicalAddress.value.streetName,
-        country: this.physicalAddress.value.country,
-        province: this.physicalAddress.value.province,
-        postalCode: this.physicalAddress.value.postalCode,
-      });
-    }
-  }
-
-  get postalAddressObj(): EmployeeAddress {
-    return {
-      id: 0,
-      unitNumber: this.postalAddress.value.unitNumber!,
-      complexName: this.postalAddress.value.complexName!,
-      suburbOrDistrict: this.postalAddress.value.suburbDistrict!,
-      city: this.postalAddress.value.city!,
-      streetNumber: this.postalAddress.value.streetNumber!,
-      streetName: this.postalAddress.value.streetName!,
-      country: this.postalAddress.value.country!,
-      province: this.postalAddress.value.province!,
-      postalCode: this.postalAddress.value.postalCode!,
-    };
   }
 
   onCountryChange(country: string): void {
@@ -460,7 +420,6 @@ export class NewEmployeeComponent implements OnInit {
   saveAddress(): void {
     combineLatest([
       this.employeeAddressService.save(this.physicalAddressObj),
-      this.employeeAddressService.save(this.postalAddressObj)
     ]).pipe(first()).subscribe()
   }
 
@@ -511,7 +470,6 @@ export class NewEmployeeComponent implements OnInit {
       ,
       dateOfBirth: this.newEmployeeForm.value.dateOfBirth,
       physicalAddress: this.physicalAddressObj,
-      postalAddress: this.postalAddressObj,
       peopleChampion: this.newEmployeeForm.controls["peopleChampion"].value == "" ? null : this.peopleChampionId
     });
 
@@ -763,7 +721,6 @@ export class NewEmployeeComponent implements OnInit {
       payRate: new FormControl(1, Validators.pattern(/^[0-9]*$/)),
       salary: new FormControl(1, Validators.pattern(/^[0-9]*$/)),
       physicalAddress: new FormControl<EmployeeAddress | null>(null),
-      postalAddress: new FormControl<EmployeeAddress | null>(null),
       peopleChampion: new FormControl<string>('')
     });
   }
